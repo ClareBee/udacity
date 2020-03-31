81232021c635c44c015490da6b21db77cfe3641b
Clare <chblackburne@gmail.com>
2020-03-31 11:28:06 +0100

    chore: delete unusedfiles utils no longer needed

--- reactnd-project-myreads-starter/src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export const formatString = originalString => {
-  const formatted = originalString.split(/(?<=[a-z])(?=[A-Z])/).join(" ");
-  const remainder = formatted.substring(1, formatted.length);
-  const capital = formatted.slice(0, 1).toUpperCase();
-  return capital + remainder;
-};
