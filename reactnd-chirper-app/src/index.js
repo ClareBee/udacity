a7e8810a71d57831ffb0ab4a981d15d656a46d03
Clare <chblackburne@gmail.com>
2020-04-01 17:11:55 +0100

    feat: add dashboard component main dashboard connected to store and rendering list of ids

--- reactnd-chirper-app/src/index.js
+++ reactnd-chirper-app/src/index.js
@@ -8,7 +8,7 @@ import reducer from "./reducers";
 import middleware from "./middleware";
 
 const store = createStore(reducer, middleware);
-
+console.log("store", store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <App />
