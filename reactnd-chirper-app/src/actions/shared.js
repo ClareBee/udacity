619b99cedb95301a3df2dbb82ae37044c8a38d87
Clare <chblackburne@gmail.com>
2020-04-01 12:54:13 +0100

    feat: set up redux actions

--- reactnd-chirper-app/src/actions/shared.js
+++ reactnd-chirper-app/src/actions/shared.js
@@ -1,12 +1,15 @@
 import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveTweets } from "./tweets";
+import { setAuthedUser } from "./authedUser";
 
+// hard-coded for demo purposes
 const AUTHED_ID = "sarah_edo";
 export function handleInitialData() {
   dispatch =>
     getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
 }
