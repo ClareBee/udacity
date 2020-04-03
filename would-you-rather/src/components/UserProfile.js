629d2cb356b23dd08250c7dbf327b6d35ab2f6e7
Clare <chblackburne@gmail.com>
2020-04-03 18:07:23 +0100

    feat: add basic leaderboard page includes user profile information ranked in descending order

--- would-you-rather/src/components/UserProfile.js
+++ would-you-rather/src/components/UserProfile.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 
-function UserProfile() {
-  return <div>User profile</div>;
+function UserProfile({ user }) {
+  console.log("user", user);
+  const { id, name, avatarURL, questions, answers } = user;
+  const numOfAnswers = Object.keys(answers).length;
+  return (
+    <div>
+      <img src={avatarURL} alt={`avatar of ${name}`} width="100" />
+      User profile: {name}
+      <span>Questions Asked: {questions.length}</span>
+      <span>Questions Answered: {numOfAnswers} </span>
+      <span>{questions.length + numOfAnswers}</span>
+    </div>
+  );
 }
 
-export default UserProfile;
+function mapStateToProps({ users }, { id }) {
+  const user = users[id];
+  return {
+    user
+  };
+}
+export default connect(mapStateToProps)(UserProfile);
--- would-you-rather/src/components/pages/Leaderboard.js
+++ would-you-rather/src/components/pages/Leaderboard.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import UserProfile from "../UserProfile";
 
-function Leaderboard() {
-  return <div>Leaderboard</div>;
+function Leaderboard({ userIds }) {
+  if (userIds.length === 0) return <div>No users</div>;
+  return (
+    <div>
+      <ul>
+        {userIds.map(userId => (
+          <UserProfile id={userId} key={userId} />
+        ))}
+      </ul>{" "}
+    </div>
+  );
 }
 
-export default Leaderboard;
+function mapStateToProps({ users }) {
+  const sumUp = user => {
+    return user.questions.length + Object.keys(user.answers).length;
+  };
+  return {
+    userIds: Object.keys(users).sort(
+      (a, b) => sumUp(users[b]) - sumUp(users[a])
+    )
+  };
+}
+export default connect(mapStateToProps)(Leaderboard);
