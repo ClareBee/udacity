61ab4cfa77648bc1ed68322f704ade11d5a93ada
Clare <chblackburne@gmail.com>
2020-04-03 10:52:32 +0100

    feat: add functionality for liking tweet

--- reactnd-chirper-app/src/actions/tweets.js
+++ reactnd-chirper-app/src/actions/tweets.js
@@ -1,6 +1,32 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 export const receiveTweets = tweets => ({
   type: RECEIVE_TWEETS,
   tweets
 });
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  };
+}
+
+export function handleToggleTweet(info) {
+  return dispatch => {
+    // for optimistic update
+    dispatch(toggleTweet(info));
+    // for api/db
+    return saveLikeToggle(info).catch(e => {
+      console.warn(`Error in handleToggleTweet: ${e}`);
+      // reset tweet
+      dispatch(toggleTweet(info));
+      alert("There was an error liking this tweet");
+    });
+  };
+}
