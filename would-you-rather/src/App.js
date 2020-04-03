c7caf1adc6993e2c85602b61e44418b7bd41c690
Clare <chblackburne@gmail.com>
2020-04-03 14:00:49 +0100

    add basic ui components

--- would-you-rather/src/App.js
+++ would-you-rather/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./components/Homepage";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <main>
+        <Homepage />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
