c3ec1dfccc5dd990d9ac4f59902e5b5dca91a83d
Clare <chblackburne@gmail.com>
2020-04-05 21:48:52 +0100

    feat: add hero pattern background from heropatterncom

--- would-you-rather/src/components/layouts/Page.js
+++ would-you-rather/src/components/layouts/Page.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 
 const PageLayout = styled.main`
   flex: 1;
-  margin: 1rem 2.5rem;
-  background: ${props => props.theme.backgroundColor};
+  margin: 1rem auto;
+  width: 60%;
+  background: ${(props) => props.theme.background};
+  padding: 1rem 2.5rem;
 `;
-const Page = props => {
+const Page = (props) => {
   return <PageLayout>{props.children}</PageLayout>;
 };
 
