2f256e0fb7c1d2d6b78006805e982f393a1f36c2
Clare <chblackburne@gmail.com>
2020-04-09 14:25:24 +0100

    feat: add slider functionality using built-in reactive native component

--- UdaciFitness/components/Slider.js
+++ UdaciFitness/components/Slider.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Slider as NativeSlider } from "react-native";
 
-function Slider() {
+function Slider({ max, unit, step, value, onChange }) {
   return (
     <View>
-      <Text>Slider</Text>
+      <NativeSlider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 }
