93afa3e1146e4cc968e6746503cf96c33f8be768
Clare <chblackburne@gmail.com>
2020-04-11 14:26:38 +0100

    feat: add tab navigation and rejig dependencies

--- UdaciFitness/App.js
+++ UdaciFitness/App.js
@@ -1,18 +1,56 @@
+import "react-native-gesture-handler";
+
 import React from "react";
-import { View } from "react-native";
+import { Platform } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { white, purple } from "./utils/colours";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={{ flex: 1 }}>
-        <View style={{ height: 20 }} />
-        <History />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ _focused, color, size }) => {
+              let icon;
+
+              if (route.name === "History") {
+                icon = (
+                  <Ionicons name="ios-bookmarks" size={size} color={color} />
+                );
+              } else if (route.name === "AddEntry") {
+                icon = (
+                  <FontAwesome name="plus-square" size={size} color={color} />
+                );
+              }
+
+              // You can return any component that you like here!
+              return icon;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: Platform.OS === "ios" ? purple : white,
+            inactiveTintColor: "gray",
+            size: 30,
+            style: {
+              height: 50,
+              backgroundColor: Platform.OS === "ios" ? white : purple,
+            },
+          }}
+        >
+          <Tab.Screen name="History" component={History} />
+          <Tab.Screen name="AddEntry" component={AddEntry} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
