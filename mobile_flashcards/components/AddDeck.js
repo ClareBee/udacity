4e04245dbab4b9be00c2ed170332d14be09f0172
Clare <chblackburne@gmail.com>
2020-04-18 14:30:47 +0100

    feat: basic form for deck title

--- mobile_flashcards/components/AddDeck.js
+++ mobile_flashcards/components/AddDeck.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { saveDeckTitle } from "../utils/api";
 
-function AddDeck() {
+function AddDeck({ navigation }) {
+  const [title, setTitle] = useState("");
+  submit = () => {
+    saveDeckTitle({ title });
+    navigation.navigate("Decks");
+
+    // clearLocalNotification().then(setLocalNotification);
+    //update redux
+
+    setTitle("");
+  };
   return (
     <View>
       <Text>Add Deck</Text>
+      <TextInput
+        value={title}
+        onChangeText={(title) => setTitle(title)}
+        placeholder={"Deck Title"}
+      />
+      <Button title={"Submit"} onPress={submit} />
     </View>
   );
 }
